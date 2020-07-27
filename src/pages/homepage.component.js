55942a2a508004032181e1aaf1d95b2a942e9130
IsmailORchid <io.thecoder@gmail.com>
2020-07-27 14:11:53 +0100

    finish header menu

--- src/pages/homepage.component.js
+++ src/pages/homepage.component.js
@@ -1,41 +1,10 @@
-import React from 'react'
+import React from 'react';
+import  Directory from '../components/directory/directory.component'
 import './homepage.style.scss'
 export default function HomePage() {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                   <div className="content">
-                      <h1 className="title">HATS</h1>
-                      <h1 className="subtitle">SHOP NOW</h1>
-                   </div>
-                </div>
-                
-                <div className="menu-item">
-                   <div className="content">
-                      <h1 className="title">HATS</h1>
-                      <h1 className="subtitle">SHOP NOW</h1>
-                   </div>
-                </div>
-                <div className="menu-item">
-                   <div className="content">
-                      <h1 className="title">HATS</h1>
-                      <h1 className="subtitle">SHOP NOW</h1>
-                   </div>
-                </div>
-                <div className="menu-item">
-                   <div className="content">
-                      <h1 className="title">HATS</h1>
-                      <h1 className="subtitle">SHOP NOW</h1>
-                   </div>
-                </div>
-                <div className="menu-item">
-                   <div className="content">
-                      <h1 className="title">HATS</h1>
-                      <h1 className="subtitle">SHOP NOW</h1>
-                   </div>
-                </div>
-            </div>
+           <Directory />
         </div>
     )
 }
