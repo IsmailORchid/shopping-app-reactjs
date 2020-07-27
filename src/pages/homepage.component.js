b19596e09a6bb325295d402d60aa9458bb78ebe7
IsmailORchid <io.thecoder@gmail.com>
2020-07-27 10:39:06 +0100

    start project

---
+++ src/pages/homepage.component.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                   <div className="content">
+                      <h1 className="title">HATS</h1>
+                      <h1 className="subtitle">SHOP NOW</h1>
+                   </div>
+                </div>
+                
+                <div className="menu-item">
+                   <div className="content">
+                      <h1 className="title">HATS</h1>
+                      <h1 className="subtitle">SHOP NOW</h1>
+                   </div>
+                </div>
+                <div className="menu-item">
+                   <div className="content">
+                      <h1 className="title">HATS</h1>
+                      <h1 className="subtitle">SHOP NOW</h1>
+                   </div>
+                </div>
+                <div className="menu-item">
+                   <div className="content">
+                      <h1 className="title">HATS</h1>
+                      <h1 className="subtitle">SHOP NOW</h1>
+                   </div>
+                </div>
+                <div className="menu-item">
+                   <div className="content">
+                      <h1 className="title">HATS</h1>
+                      <h1 className="subtitle">SHOP NOW</h1>
+                   </div>
+                </div>
+            </div>
+        </div>
+    )
+}
